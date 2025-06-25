c897e686fab99d6913d5b3393f4da1a234f66429
Alex Reyes <alex.reyes.o@mail.pucv.cl>
2025-06-24 23:01:20 -0400

    Update tragamonedas.h
    listo

--- tragamonedas.h
+++ tragamonedas.h
@@ -1,5 +1,8 @@
 #ifndef TRAGAMONEDAS_H
 #define TRAGAMONEDAS_H
 
+void inicializarTablaHash();
+float jugarTragamonedas(float apuesta);
+void mostrarPantalla(const char *e1, const char *e2, const char *e3);
 
-#endif 
+#endif
