2cb31443151956d88f59e9b502fe3e3ef584b88f
leandrobravo24 <leandro.bravo.g@mail.pucv.cl>
2025-06-25 21:18:29 -0400

    Update stack.h

--- tdas/stack.h
+++ tdas/stack.h
@@ -1,17 +1,37 @@
 #ifndef STACK_H
 #define STACK_H
+
+#include <stddef.h>
 #include "list.h"
 
 typedef List Stack;
 
-Stack *stack_create(Stack *stack) { return list_create(); }
+static inline Stack *stack_create() {
+    return list_create();
+}
 
-void stack_push(Stack *stack, void *data) { list_pushFront(stack, data); }
+static inline void stack_push(Stack *stack, void *data) {
+    list_pushBack(stack, data);
+}
 
-void *stack_top(Stack *stack) { return list_first(stack); }
+static inline void *stack_pop(Stack *stack) {
+    return list_popBack(stack);
+}
 
-void *stack_pop(Stack *stack) { return list_popFront(stack); }
+static inline void *stack_top(Stack *stack) {
+    if (list_size(stack) == 0) return NULL;
+    void *elem = list_first(stack);
+    for (int i = 1; i < list_size(stack); ++i)
+        elem = list_next(stack);
+    return elem;
+}
 
-void stack_clean(Stack *stack) { list_clean(stack); }
+static inline int stack_size(Stack *stack) {
+    return list_size(stack);
+}
 
-#endif /* STACK_H */
+static inline void stack_clean(Stack *stack) {
+    list_clean(stack);
+}
+
+#endif
